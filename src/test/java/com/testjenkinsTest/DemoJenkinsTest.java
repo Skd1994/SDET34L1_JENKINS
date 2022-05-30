942919f477dc059bd42798333e8707575fe6bc9d
Admin <Admin@DESKTOP-7UUT0JG>
2022-05-30 15:15:53 +0530

    jenkins first commit

---
+++ src/test/java/com/testjenkinsTest/DemoJenkinsTest.java
@@ -0,0 +1,12 @@
+package com.testjenkinsTest;
+
+import org.testng.annotations.Test;
+
+public class DemoJenkinsTest {
+	@Test
+	public void practiceTest1()
+	{
+		System.out.println("running");
+	}
+
+}
